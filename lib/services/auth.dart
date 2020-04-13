acd2395504cb05107360cf6215abcf284c334e60
Carlos  Moyano <carlosmoyano@Carloss-MacBook-Pro.local>
2020-04-13 13:57:34 -0400

    deleted background for loading

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:test_device/models/user.dart';
 import 'package:test_device/services/database.dart';
@@ -7,10 +7,63 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final database = FirebaseDatabase.instance;
+
+  void handleRTB(String uid) {
+    try {
+      var userStatusDatabaseRef = database.reference().child('/status/' + uid);
+
+// We'll create two constants which we will write to
+// the Realtime database when this device is offline
+// or online.
+      var isOfflineForDatabase = {
+        "state": 'offline',
+        "last_changed": ServerValue.timestamp
+      };
+
+      var isOnlineForDatabase = {
+        "state": 'online',
+        "last_changed": ServerValue.timestamp,
+      };
+
+// Create a reference to the special '.info/connected' path in
+// Realtime Database. This path returns `true` when connected
+// and `false` when disconnected.
+      final infoRef = database.reference().child('.info/connected');
+      infoRef.once().then((DataSnapshot snapshot) async {
+        // If we're not currently connected, don't do anything.
+        if (snapshot.value == false) {
+          return;
+        }
+
+        // If we are currently connected, then use the 'onDisconnect()'
+        // method to add a set which will only trigger once this
+        // client has disconnected by closing the app,
+        // losing internet, or any other means.
+        await userStatusDatabaseRef.onDisconnect().set(isOfflineForDatabase);
+        // The promise returned from .onDisconnect().set() will
+        // resolve as soon as the server acknowledges the onDisconnect()
+        // request, NOT once we've actually disconnected:
+        // https://firebase.google.com/docs/reference/js/firebase.database.OnDisconnect
+
+        // We can now safely set ourselves as 'online' knowing that the
+        // server will mark us as offline once we lose connection.
+        await userStatusDatabaseRef.set(isOnlineForDatabase);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
 
   // create user obj based on FirebaseUser
   User _userFromFirebaseUser(FirebaseUser user) {
-    return user != null ? User(uid: user.uid) : null;
+    try {
+      /*    handleRTB(user.uid); */
+      return user != null ? User(uid: user.uid) : null;
+    } catch (e) {
+      print(e);
+      return User();
+    }
   }
 
   //auth change user stream
--- lib/shared/loading.dart
+++ lib/shared/loading.dart
@@ -5,20 +5,6 @@ class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color(0xFFd0d6b5),
-            Color(0xFF75b9be),
-            Color(0xFF987284),
-          ],
-          stops: [
-            0,
-            55,
-            100,
-          ],
-        ),
-      ),
       child: Center(
         child: SizedBox(
           child: SpinKitChasingDots(color: Theme.of(context).primaryColor),
